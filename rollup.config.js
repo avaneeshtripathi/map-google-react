e606571691a8c55b58982d3d894aa15e9ff4e4d7
avaneeshtripathi <avaneeshtripathi93@gmail.com>
2020-08-18 00:23:45 +0530

    chore: update config and add rollup got builds

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import { terser } from "rollup-plugin-terser";
+import sass from 'rollup-plugin-sass';
+import bundleSize from 'rollup-plugin-bundle-size';
+import cleanup from 'rollup-plugin-cleanup';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false
+    }
+  ],
+  plugins: [
+    sass({ insert: true }),
+    typescript({ objectHashIgnoreUnknownHack: false }),
+    // cleanup(),
+    // terser(),
+    bundleSize(),
+  ],
+  external: ['react', 'react-dom']
+}
